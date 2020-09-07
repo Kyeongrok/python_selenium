17b20d4da0c6a328be77c88085c8cfa3bed33c88
Kyeongrok <oceanfog1@gmail.com>
2020-09-07 20:34:23 +0900

    type id

--- baemin/01_crawl.py
+++ baemin/01_crawl.py
@@ -7,10 +7,14 @@ chrome_options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
 driver = webdriver.Chrome('../chrome/chromedriver', options=chrome_options)
 
 # 디버그 모드 chrome에서 로그인을 하신 후 실행 하세요.
-url = 'https://ceo.baemin.com/self-service/orders/tax'
 
+url = 'https://ceo.baemin.com/'
 driver.get(url)
 
+driver.find_element_by_xpath('//*[@id="id"]').send_keys('oceanfog')
+
+url = 'https://ceo.baemin.com/self-service/orders/tax'
+driver.get(url)
 # 2분기
 time.sleep(2)
 driver.find_element_by_xpath('//*[@id="root"]/div/div[1]/div[3]/div[1]/div[1]/div/div[1]/label[2]/p').click()
