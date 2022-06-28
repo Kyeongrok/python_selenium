2d56a78699c7b7caa401f1e8de377edeebe99ffa
Kyeongrok <oceanfog1@gmail.com>
2022-06-28 20:06:28 +0900

    selenium 4.3.0에 맞게 마이그레이션

--- godpia/01_godpia.py
+++ godpia/01_godpia.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.keys import Keys
 import random, os
 
 import chromedriver_autoinstaller
+from selenium.webdriver.common.by import By
 
 class GodPiaBibleWriter:
 
@@ -30,36 +31,35 @@ class GodPiaBibleWriter:
     def login(self, driver, id, password):
         url = 'http://www.godpia.com/login/login_page.asp?ishref=http://bible.godpia.com/frameindex.asp?url_flag=/index.asp?&ipserver=bible.godpia.com'
         driver.get(url)
-        time.sleep(1)
-        driver.find_element_by_id("inputID") \
+
+        driver.find_element(By.ID, "inputID") \
             .send_keys(id)
-        driver.find_element_by_id("inputPW") \
+        driver.find_element(By.ID, "inputPW") \
             .send_keys(password)
 
         xp = '/html/body/table/tbody/tr[2]/td/table/tbody/tr/td/table/tbody/tr[3]/td/table/tbody/tr[1]/td[1]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr/td[2]/table/tbody/tr[2]/td/a/img'
-        driver.find_element_by_xpath(xp).click()
+        driver.find_element(By.XPATH, xp).click()
 
     def run(self, chapterUrl):
         self.driver.get(chapterUrl)
 
-        ul = self.driver.find_element_by_class_name("write").find_element_by_tag_name("ul")
-        lis = ul\
-            .find_elements_by_tag_name("li")
+        ul = self.driver.find_element(By.CLASS_NAME,"write").find_element(By.TAG_NAME, "ul")
+        lis = ul.find_elements(By.TAG_NAME, "li")
 
         print("절 수:", len(lis))
 
         for index in range(0, len(lis)):
             li = lis[index]
-            ps = li.find_elements_by_tag_name("p")
+            ps = li.find_elements(By.TAG_NAME, "p")
             statement = ps[0].text
             rndInt = random.randint(2, 5)
             print("{}절 중 {}절 : rndSec:{}".format(len(lis), index+1, rndInt))
-            textarea = ps[1].find_element_by_tag_name("textarea")
+            textarea = ps[1].find_element(By.TAG_NAME, "textarea")
             time.sleep(rndInt)
 
             cnt = 0
             while (textarea.is_enabled() == False and cnt < 5):
-                textarea = ps[1].find_element_by_tag_name("textarea")
+                textarea = ps[1].find_element(By.TAG_NAME, "textarea")
                 textarea.click()
                 rndInt = random.randint(2, 5)
                 print(f"------is_enabled() False then plus {rndInt} sec -----")
@@ -81,12 +81,12 @@ class GodPiaBibleWriter:
             print("{}장".format(chapter))
             chapterUrl = f"http://bible.godpia.com/write/{sub}.asp?cb_idx={cb_idx}&ver=gae&vol={book_cd}&chap={chapter}&secindex=1"
             self.run(chapterUrl)
-            time.sleep(60 * 6)
+            time.sleep(30)
 
 #1pe 벧전 #2pe벧후3 1jn요일 5 계rev
 #창gen
 godpia_writer = GodPiaBibleWriter('login')
-godpia_writer.call('sub020302', '2386', 'gen', 26, 27)
+godpia_writer.call('sub020302', '2386', 'gen', 10, 11)
 
 
 
